59bafd178426a063a67798801699345c987f8957
Nathan Papes <you@example.com>
2016-01-17 22:29:55 -0500

    Refactor nested for-loops and added caching for efficiently
    Fix methods args documentation

--- unread_msg_monitor.py
+++ unread_msg_monitor.py
@@ -16,17 +16,21 @@ Generate your token at https://api.slack.com/web and add it to TOKEN variable.
 To enable this program on start up, add the something like in /etc/rc.local:
   . /home/papes1ns/.virtualenvs/unread/bin/activate &&
   python /home/papes1ns/Projects/hes_slack_integration/unread_msg_monitor.py &
+
+With 5 sec refresh rate, best case ~14 requests in 1 minute,
+worst case ~113 requests in 1 minute.
 """
 
 import requests
 import sys
-import os
+from os.path import isfile
 from subprocess import call
 from time import sleep
 
 # user defined settings
-TOKEN = ""
-G9LED_PATH = "/home/papes1ns/Projects/g9led"  # where did you put g9led executable?
+TOKEN = "xoxp-15777566643-18203039952-18684119408-619ec1e889"
+G9LED_PATH = "/home/papes1ns/Projects/g9led"  # where did you put g9led?
+REFRESH_RATE = 5  # sleep for how many seconds between requests?
 
 # has_unread G9 mouse color mapping
 # can use any hex value for color
@@ -49,29 +53,69 @@ API_METHODS = {
 SLACK_API_URL = "https://slack.com/api/"
 
 
+def update_led(has_unread):
+    # has_unread: bool
+    call(G9LED_PATH + " %s" % COLORS[str(has_unread)], shell=True)
+
+
+def get_channel_ids(method, kwargs):
+    # method: string
+    # kwargs: dict
+
+    try:
+        r = requests.post(SLACK_API_URL + method, {"token": TOKEN})
+    except requests.exceptions.ConnectionError:
+        sys.stderr.write("Could not connect to slack.\n")
+        return []
+    return [c.get("id", None) for c in r.json().get(kwargs["key"], [])]
+
+
+def has_unread(channel_ids, kwargs, unread_id):
+    # channel_ids: list
+    # kwargs: dict
+    # unread_id: sting
+
+    if unread_id is not None:
+        channel_ids = [unread_id]
+
+    for channel_id in channel_ids:
+        payload = {"token": TOKEN,
+                   "channel": channel_id,
+                   "unreads": 1}
+        try:
+            r = requests.post(SLACK_API_URL + kwargs["history"], payload)
+        except requests.exceptions.ConnectionError:
+            sys.stderr.write("Could not connect to slack.\n")
+            break
+
+        if r.json().get("unread_count_display", 0) > 0:
+            return channel_id
+    return None
+
+
 def start_service():
+    channel_ids = []
+    half_chans = True
+    unread_id = None  # cache last unread channel id to be efficient
     while True:
-        has_unread = False
         for method, kwargs in API_METHODS.iteritems():
-            try:
-                r = requests.post(SLACK_API_URL + method, {"token": TOKEN})
-                im_channels = [c.get("id", None) for c in r.json().get(kwargs["key"], [])]
-                for channel_id in im_channels:
-                    payload = {"token": TOKEN,
-                               "channel": channel_id,
-                               "unreads": 1}
-                    r = requests.post(SLACK_API_URL + kwargs["history"], payload)
-                    if r.json().get("unread_count_display", 0) > 0:
-                        has_unread = True
+            # get all im and normal channels once to be efficient
+            if not channel_ids:
+                channel_ids = get_channel_ids(method, kwargs)
+            elif half_chans:
+                channel_ids.append(get_channel_ids(method, kwargs))
+                half_chans = False
 
-            except requests.exceptions.ConnectionError:
-                sys.stderr.write("Could not connect to slack.\n")
+            unread_id = has_unread(channel_ids, kwargs, unread_id)
+            if unread_id:
+                break  # avoid LED flicker and avoid unneeded POST to slack
 
-        call(G9LED_PATH + " " + COLORS[str(has_unread)], shell=True)
-        sleep(1)  # let slack server rest for a second.. literally
+        update_led(bool(unread_id))
+        sleep(REFRESH_RATE)  # allow slack server to rest
+
 
 if __name__ == "__main__":
-    if not os.path.isfile(G9LED_PATH):
+    if not isfile(G9LED_PATH):
         sys.stderr.write("Could not find g9led executable in %s\n" % G9LED_PATH)
         sys.exit(1)
 
