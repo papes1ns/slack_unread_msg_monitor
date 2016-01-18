2655e1b5595a7bf5ccd7d5d49073dc811034cd1d
Nathan Papes <you@example.com>
2016-01-17 20:25:34 -0500

    Add error checking and adhere to DRY and conform to pep8 (mostly)

--- unread_msg_monitor.py
+++ unread_msg_monitor.py
@@ -1,29 +1,31 @@
 """ Program that changes the LED color of Logitech G9 mouse based on slack
 unread message count. If there's unread messages, change LED to white, if
-there's no message, simply turn off the light.
+there's no message, turn off the light. Checks all IMS and channels subscribed.
 
 This program relies on the g9led executable, can be downloaded at:
   http://als.regnet.cz/logitech-g9-linux-led-color.html
 
-Once g9led is downloaded and compiled, find a home for it and specify it's location in G9LED variable.
+Once g9led is downloaded and compiled, find a home for it and specify it's
+location in G9LED variable.
+
 g9led requires sudo to successfully change LED color, so run script like:
   sudo python unread_msg_monitor.py
 
 Generate your token at https://api.slack.com/web and add it to TOKEN variable.
 
-To enable this program on start up, add the something like the following to /etc/rc.local:
-  . /home/papes1ns/.virtualenvs/unread/bin/activate && python /home/papes1ns/Projects/hes_slack_integration/unread_msg_monitor.py &
-
-Be sure to include the "." in the above command to source the enviroment.
+To enable this program on start up, add the something like in /etc/rc.local:
+  . /home/papes1ns/.virtualenvs/unread/bin/activate &&
+  python /home/papes1ns/Projects/hes_slack_integration/unread_msg_monitor.py &
 """
 
 import requests
 import sys
+import os
 from subprocess import call
 from time import sleep
 
-SLACK_API_URL = "https://slack.com/api/"  # don't change
-TOKEN = ""  # token can be generated at https://api.slack.com/web
+SLACK_API_URL = "https://slack.com/api/"
+TOKEN = ""
 G9LED_PATH = "/home/papes1ns/Projects/"  # where did you put g9led executable?
 
 # has_unread G9 mouse color mapping
@@ -31,37 +33,49 @@ G9LED_PATH = "/home/papes1ns/Projects/"  # where did you put g9led executable?
 COLORS = {"True": "FFFFFF",  # white
           "False": "000000"}  # no light
 
+# keys are slack api methods, values are keys to extract the data wanted
+# https://api.slack.com/methods
+API_METHODS = {
+    "im.list": {
+        "key": "ims",
+        "history": "im.history"
+    },
+    "channels.list": {
+        "key": "channels",
+        "history": "channels.history"
+    }
+}
+
 
 def start_service():
     while True:
         has_unread = False
+        for method, kwargs in API_METHODS.iteritems():
+            try:
+                r = requests.post(SLACK_API_URL + method, {"token": TOKEN})
+                im_channels = [c.get("id", None) for c in r.json().get(kwargs["key"], [])]
+                for channel_id in im_channels:
+                    payload = {"token": TOKEN,
+                               "channel": channel_id,
+                               "unreads": 1}
+                    r = requests.post(SLACK_API_URL + kwargs["history"], payload)
+                    if r.json().get("unread_count_display", 0) > 0:
+                        has_unread = True
 
-        try:
-            # check for unread instant messages
-            r = requests.post(SLACK_API_URL+"im.list", {"token": TOKEN})
-            im_channels = [c.get("id", None) for c in r.json().get("ims", [])]
-            for channel_id in im_channels:
-                r = requests.post(SLACK_API_URL+"im.history", {"token": TOKEN,
-                                                               "channel": channel_id,
-                                                               "unreads": 1})
-                if r.json().get("unread_count_display", 0) > 0:
-                    has_unread = True
-
-            # check for unread channel posts
-            r = requests.post(SLACK_API_URL+"channels.list", {"token": TOKEN})
-            channels = [c.get("id", None) for c in r.json().get("channels", [])]
-            for channel_id in channels:
-                r = requests.post(SLACK_API_URL+"channels.history", {"token": TOKEN,
-                                                                     "channel": channel_id,
-                                                                     "unreads": 1})
-                if r.json().get("unread_count_display", 0) > 0:
-                    has_unread = True
-
-        except requests.exceptions.ConnectionError:
-            sys.stderr.write("Could not connect to slack.\n")
+            except requests.exceptions.ConnectionError:
+                sys.stderr.write("Could not connect to slack.\n")
 
         call(G9LED_PATH + "g9led " + COLORS[str(has_unread)], shell=True)
         sleep(1)  # let slack server rest for a second.. literally
 
 if __name__ == "__main__":
+    if not os.path.isfile(G9LED_PATH + "g9led"):
+        sys.stderr.write("Could not find g9led executable in %s\n" % G9LED_PATH)
+        sys.exit(1)
+
+    if not TOKEN:
+        sys.stderr.write("Please generate a token at https://api.slack.com/web " \
+                          "and set the TOKEN value in the python script.\n")
+        sys.exit(1)
+
     start_service()
